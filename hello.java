666b1e910a2d008133039ae27a3aea4dc5f3db64
Vinaywagh123 <waghvinay540@gmail.com>
2024-12-29 23:52:18 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ public class hello
     public static void main(String args[])
     {
       System.out.println("Vinay Wagh");
-      for(int i=2; i<20; i+2)
+      for(int i=2; i<=20; i+=2)
         {
           System.out.println(i);
         }
